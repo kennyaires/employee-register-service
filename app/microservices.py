634a182a93121d2ec71bd34f8a6c74fc6317a170
Kenny <kennyaires@gmail.com>
2020-03-12 06:21:35 -0300

    Add JWT authentication to the api

--- app/microservices.py
+++ app/microservices.py
@@ -6,7 +6,7 @@ from app.settings import ADDRESS_SERVICE_URL, ADDRESS_SERVICE_AUTH_TOKEN
 def get_full_address(postal_code):
     """Call microservice to get address details from postal code"""
     response = requests.get(
-        url=f'{ADDRESS_SERVICE_URL}/{postal_code}/json',
+        url=f'{ADDRESS_SERVICE_URL}/{postal_code}',
         headers={'Authorization': f'Bearer {ADDRESS_SERVICE_AUTH_TOKEN}'},
     )
     if response:
